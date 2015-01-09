ac20aecd22d6fcb268eca23251bdde094abb1a02
Delmer Reed <delmer814@yahoo.com>
2015-01-09 13:49:56 -0800

    typhoeus

--- app/models/creature.rb
+++ app/models/creature.rb
@@ -1,2 +1,11 @@
 class Creature < ActiveRecord::Base
+
+  def make_request
+    res = Typhoeus.get("www.google.com/search", {
+        params: {
+          q:  @name
+        }
+      })
+    res.body
+  end
 end
